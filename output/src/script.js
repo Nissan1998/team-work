084e8cf8359421d8c10e4ba9385c031cc934808c
Nissan1998 <jnnissanm@gmail.com>
2023-08-01 15:59:06 +0600

    first commit

---
+++ output/src/script.js
@@ -0,0 +1,4 @@
+"use strict";
+const country = "bangladesh";
+console.log(country);
+const date = 25;
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+const player = (name: string, age: number) => {
+  console.log(name, age);
+};
+const result = player("sakib", 38);
+console.log(result);
